baae26a00244cf27600bd670431d417ee5376b42
Daniel Saukel <Sataniel98@users.noreply.github.com>
2020-06-02 19:45:16 +0200

    Don't forbid OPs to use commands

--- src/main/java/de/erethon/jaildre/JailDRE.java
+++ src/main/java/de/erethon/jaildre/JailDRE.java
@@ -130,9 +130,6 @@ public class JailDRE extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
         Player player = event.getPlayer();
-        if (player.isOp()) {
-            return;
-        }
         int time = getJailTime(player);
         if (time <= 0) {
             return;
